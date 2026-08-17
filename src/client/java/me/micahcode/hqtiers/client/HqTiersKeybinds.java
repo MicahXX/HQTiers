af3dd58b793ddad04823019a07a4b06ee7d086f4
Marti <shirogg10@gmail.com>
2026-08-17 12:42:35 +0200

    new keybinds

--- src/client/java/me/micahcode/hqtiers/client/HqTiersKeybinds.java
+++ src/client/java/me/micahcode/hqtiers/client/HqTiersKeybinds.java
@@ -43,6 +43,18 @@ public final class HqTiersKeybinds {
                 "category.hqtiers"
         ));
 
+        KeyMapping tablist = KeyMappingHelper.registerKeyMapping(HqTiersMinecraftCompat.keyBinding(
+                "key.hqtiers.tablist",
+                InputConstants.KEY_T,
+                "category.hqtiers"
+        ));
+
+        KeyMapping nametag = KeyMappingHelper.registerKeyMapping(HqTiersMinecraftCompat.keyBinding(
+                "key.hqtiers.nametag",
+                InputConstants.KEY_N,
+                "category.hqtiers"
+        ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             unbindAdvancementsIfConflicting(client.options, leaderboard);
 
@@ -70,6 +82,14 @@ public final class HqTiersKeybinds {
                             client.player.getName().getString()
                     ));
                 }
+            }
+
+            while (tablist.consumeClick()) {
+                toggleTablist(client);
+            }
+
+            while (nametag.consumeClick()) {
+                toggleNametag(client);
             }
         });
     }
@@ -117,6 +137,30 @@ public final class HqTiersKeybinds {
             String iconLadderName = next instanceof HqTiersLadder ladder ? ladder.name() : HqTiersLadder.GLOBAL.name();
             msg.append(HqTiersFormatter.icon(iconLadderName));
 
+            client.gui.hud.setOverlayMessage(msg, false);
+        }
+    }
+
+    private static void toggleTablist(net.minecraft.client.Minecraft client) {
+        HqTiersClientConfig.tabListEnabled = !HqTiersClientConfig.tabListEnabled;
+        HqTiersClientConfig.save();
+
+        if (client.player != null) {
+            net.minecraft.network.chat.MutableComponent msg = net.minecraft.network.chat.Component.literal(
+                    "HQTiers: Tablist stats " + (HqTiersClientConfig.tabListEnabled ? "enabled" : "disabled")
+            ).withStyle(ChatFormatting.GOLD);
+            client.gui.hud.setOverlayMessage(msg, false);
+        }
+    }
+
+    private static void toggleNametag(net.minecraft.client.Minecraft client) {
+        HqTiersClientConfig.nametagEnabled = !HqTiersClientConfig.nametagEnabled;
+        HqTiersClientConfig.save();
+
+        if (client.player != null) {
+            net.minecraft.network.chat.MutableComponent msg = net.minecraft.network.chat.Component.literal(
+                    "HQTiers: Nametag stats " + (HqTiersClientConfig.nametagEnabled ? "enabled" : "disabled")
+            ).withStyle(ChatFormatting.GOLD);
             client.gui.hud.setOverlayMessage(msg, false);
         }
     }
